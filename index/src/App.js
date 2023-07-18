ef6fc02d595dec3605e536f2860a90c18ba8d7f7
lantun01 <49689697+lantun01@users.noreply.github.com>
2023-07-17 23:24:44 -0400

    Revert "feat<App>:Add basic MainTable"
    This reverts commit d91a2806ef3ab2efdd08be4372612de5c5ea33da.

--- index/src/App.js
+++ index/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import MainTable from './components/MainTable';
+
 import Header from './template/Header';
-import './App.css'
 
-
-function App() {
+const App = () => {
     return (
-        <div className='header-container'>
-            <Header/>
-            <div className='table-container'>
-                <MainTable/>
-            </div>
-        </div>
-    );
+        <Header></Header>
+    )
 }
 
 export default App;
